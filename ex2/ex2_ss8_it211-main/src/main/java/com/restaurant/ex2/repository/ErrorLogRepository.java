d89c2c9c4259daa1c488991b7727c9388cca85a8
Longle183 <lethanhlong1224@gmail.com>
2026-05-24 21:41:13 +0700

    Delete ex2/ex2_ss8_it211-main directory

--- ex2/ex2_ss8_it211-main/src/main/java/com/restaurant/ex2/repository/ErrorLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restaurant.ex2.repository;
-
-import com.restaurant.ex2.entity.ErrorLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ErrorLogRepository extends JpaRepository<ErrorLog, Long> {
-}
--- ex2/ex2_ss8_it211-main/src/main/java/com/restaurant/ex2/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restaurant.ex2.repository;
-
-import com.restaurant.ex2.entity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
